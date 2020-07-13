2fc280dc9241782d763d7d988ef70461555b8ff5
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-07-13 22:13:23 +0300

    Mira22

---
+++ Moba 2023/Assets/Scripts/Peekcam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peekcam : MonoBehaviour
+{
+    public Transform pl; // Player
+    public int offset; // Distance between cam and player
+    public float smooth; // Cam zoom smoothness
+
+    private float fov = 60; // Field of view
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(pl.position.x, pl.position.y + offset, pl.position.z - offset / 2 - 2.5f);
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && fov > 30) fov -= 5;
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && fov < 70) fov += 5;  
+        
+        GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, fov, Time.deltaTime * smooth);
+    }
+}
