13ba51daad95de54eda9f71ca325ba8f8c3b252c
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-07-14 18:46:59 +0300

    s

--- Moba 2023/Assets/Scripts/PlayerControl.cs
+++ Moba 2023/Assets/Scripts/PlayerControl.cs
@@ -8,8 +8,10 @@ public class PlayerControl : MonoBehaviour
 {
     NavMeshAgent agent;
     Animator anim_Cont;
+    string state = "";//idle, move, chasing
+    public float range;
+    public Transform enemy;
 
-    public string animState = "idle";
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -25,31 +27,78 @@ public class PlayerControl : MonoBehaviour
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-                agent.destination = hit.point;
-                anim_Cont.SetBool("moving", true);
+                if (hit.transform.tag == "Enemy")
+                {
+                    state = "chasing";//DUSMANI KOVALIYOR YADA SALDIRIRYOR
+                    enemy = hit.transform;
+                }
+                else
+                {
+                    state = "move";
+                    agent.destination = hit.point;
+                    AnimHandler("move");
+                }
             }
         }
 
-        // Check the distance between destination and agent 
-        if (Vector3.Distance(agent.destination, agent.transform.position) < 0.5f)
+        if (state == "move")
         {
-            anim_Cont.SetBool("moving", false);
+            if (Vector3.Distance(agent.destination, agent.transform.position) < 0.5f)
+            {
+                state = "idle";
+                AnimHandler("idle");
+            }
         }
+        else if (state == "chasing")
+        {
+            if (Vector3.Distance(enemy.position, agent.transform.position) < range)
+            {
+                agent.ResetPath();
+                AnimHandler("attack");
+            }
+            else
+            {
+                agent.destination = enemy.position;
+                AnimHandler("move");
+            }
+        }
+
+
+        /*
+        if (Input.GetKeyDown("1"))
+        {
+            AnimHandler("idle");
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            AnimHandler("move");
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            AnimHandler("attack");
+        }*/
     }
 
-    void AnimHandler()
+    void AnimHandler(string animState)
     {
         if (animState == "idle")
         {
-            anim_Cont.SetBool("moving", false);
+            anim_Cont.SetBool("idle", true);
+            anim_Cont.SetBool("move", false);
+            anim_Cont.SetBool("attack", false);
         }
         else if (animState == "move")
         {
-            anim_Cont.SetBool("moving", false);
+            anim_Cont.SetBool("idle", false);
+            anim_Cont.SetBool("move", true);
+            anim_Cont.SetBool("attack", false);
         }
         else
         {
-            anim_Cont.SetBool("attack", false);
+            anim_Cont.SetBool("idle", false);
+            anim_Cont.SetBool("move", false);
+            anim_Cont.SetBool("attack", true);
         }
     }
+
 }
