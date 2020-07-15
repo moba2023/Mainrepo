04c1b73e7ee54517bda41ed3e3bf1aec49f2f24d
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-07-15 17:17:55 +0300

    s

---
+++ Moba 2023/Assets/Scripts/Statsinfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statsinfo : MonoBehaviour
+{
+
+    [Header("Vigor")]
+
+    public int health;
+    private int attackPower;
+}
