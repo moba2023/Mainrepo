0086bdefc01bda2f92a77afcf12693ac82ca4384
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-07-24 19:38:42 +0300

    Outline shader

---
+++ Moba 2023/Assets/GameHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+
+    private Renderer currRenderer;
+    private Renderer oldRenderer;
+    private void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, LayerMask.GetMask("Terrain")))
+        {
+            if (hit.collider.tag == "Any Team")
+            {
+                if (currRenderer != null && currRenderer != hit.collider.transform.Find("Mesh").GetComponent<Renderer>())
+                {
+                    Hide();
+                    currRenderer = hit.collider.transform.Find("Mesh").GetComponent<Renderer>();
+                    Show();
+                }
+                currRenderer = hit.collider.transform.Find("Mesh").GetComponent<Renderer>();
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+                
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    public void Show()
+    {
+        if (currRenderer != null)
+        {
+            currRenderer.material.SetFloat("_Outline", 0.1f);
+        }
+    }
+
+    public void Hide()
+    {
+        if (currRenderer != null)
+        {
+            currRenderer.material.SetFloat("_Outline", 0);
+            currRenderer = null;
+        }
+    }
+}
