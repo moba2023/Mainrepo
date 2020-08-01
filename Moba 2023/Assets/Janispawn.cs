b745e22ad437609276db7ad3710a3078a3c056ac
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-08-01 22:54:51 +0300

    melee minion

--- Moba 2023/Assets/Janispawn.cs
+++ Moba 2023/Assets/Janispawn.cs
@@ -6,31 +6,49 @@ public class Janispawn : MonoBehaviour
 {
 
     public GameObject Janis; // prefab
+    public GameObject Janismelee;
     public Transform point;
     public Transform beys;
 
-    private int spCount = 0;
+    private int sprCount = 0;
+    private int spmCount = 0;
+
 
     private void Start()
     {
-        InvokeRepeating("Spawnvawe", 2, 30);
+        InvokeRepeating("Spawnvawe", 2, 40);
     }
 
 
     void Spawnvawe()
     {
-        InvokeRepeating("Spawn", 0, 1.5f);
+        InvokeRepeating("SpawnMelee", 0, 1.5f);
     }
 
-    void Spawn()
+
+    void SpawnRanged()
     {
         GameObject jani = Instantiate(Janis, transform.position, Quaternion.identity);
         jani.transform.GetChild(0).GetComponent<Janisarry>().nextPoint = point;
         jani.transform.GetChild(0).GetComponent<Janisarry>().targetBase = beys;
-        if (++spCount%3 ==0)
+        jani.transform.SetParent(GameObject.Find("Minions").transform);
+        if (++sprCount%3 ==0)
         {
-            CancelInvoke("Spawn");
+            CancelInvoke("SpawnRanged");
+            
         }
+    }
 
+    void SpawnMelee()
+    {
+        GameObject jani = Instantiate(Janismelee, transform.position, Quaternion.identity);
+        jani.transform.GetChild(0).GetComponent<Janisarry>().nextPoint = point;
+        jani.transform.GetChild(0).GetComponent<Janisarry>().targetBase = beys;
+        jani.transform.SetParent(GameObject.Find("Minions").transform);
+        if (++spmCount % 3 == 0)
+        {
+            CancelInvoke("SpawnMelee");
+            InvokeRepeating("SpawnRanged", 3, 1.5f);
+        }
     }
 }
---
+++ Moba 2023/Assets/Scripts/Janissary/Janirangedanimevent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Janirangedanimevent : MonoBehaviour
+{
+    Statsinfo stats;
+
+    private void Start()
+    {
+        stats = transform.parent.GetComponent<Statsinfo>();
+    }
+
+    private void Update()
+    {
+        GetComponent<CapsuleCollider>().center = transform.Find("mixamorig:Hips").localPosition;
+    }
+
+    public void BeginAttack()
+    {
+        if (stats.target != null)
+        {
+
+        }
+    }
+
+    public void MidAttack()
+    {
+        if (stats.target != null)
+        {
+            stats.target.parent.GetComponent<Statsinfo>().curHealth -= 40;
+        }
+    }
+
+
+    public void EndAttack()
+    {
+        GetComponent<Janisarry>().chaseBreak = false;
+    }
+}
